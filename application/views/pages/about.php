a2b7cbad7777e131e1a12a7de4e898c94822da56
Arturs5447 <remontasnb@gmail.com>
2018-07-19 16:41:26 +0300

    atnaujinimas

---
+++ application/views/pages/about.php
@@ -0,0 +1,2 @@
+<h2><?php echo $title ?></h2>
+<P>version 1.0</P>
---
+++ application/views/templates/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>RMA</title>
+	<!-- <link rel="stylesheet" type="text/css" href="https://bootswatch.com/4/minty/bootstrap.min.css"> -->
+	<link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+</head>
+<body>
+	
+<nav class="navbar navbar-inverse">
+	<div class="container">
+		<div class="navbar-header">
+			<a href="<?php echo base_url(); ?>" class="navbar-brand">RMA</a>
+		</div>
+		<div id="navbar">
+			<ul class="nav navbar-nav">
+				<li><a href="<?php echo base_url(); ?>">Home</a></li>
+				<li><a href="<?php echo base_url(); ?>about">About</a></li>
+				<li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
+
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<div class="container">
