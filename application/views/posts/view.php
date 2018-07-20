34fc83eccde4346553fb7171aaae2b5c50078bae
Arturs5447 <remontasnb@gmail.com>
2018-07-20 18:31:23 +0300

    update

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -3,3 +3,11 @@
 <div class="post-body">
 	<?php echo $post['body']; ?>
 </div>
+
+<hr>
+
+<?php echo form_open('/posts/delete/'.$post['id']); ?>
+<input type="submit" value="Delete" class="btn btn-danger">
+</form>
+<br>
+<a href="<?php echo base_url(); ?>posts/edit/<?php echo $post['slug']; ?>" class="btn btn-info">Edit</a>
